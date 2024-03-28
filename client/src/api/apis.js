4c2613169c017366ba0319681de2a1071e2bc40d
amirnd51 <amirnass@buffalo.edu>
2024-03-27 23:50:27 -0400

    Delete unnecessary audio and image files and update gitignore

--- client/src/api/apis.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const apiUrl = `${process.env.REACT_APP_SERVER_URL}` +  ":" + `${process.env.REACT_APP_SERVER_PORT}`
-
-console.log(apiUrl);
-
-export const api = axios.create({
-    baseURL: apiUrl
-});
